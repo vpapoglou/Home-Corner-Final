da650867a3df227fcca99cacd4f79e41e4f9c8e0
vpapoglou <vpapoglou@gmail.com>
2019-05-04 15:05:35 +0300

    Add project files.

---
+++ HomeCorner2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HomeCorner2.MvcApplication" Language="C#" %>
---
+++ HomeCorner2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeCorner2.Startup))]
+namespace HomeCorner2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
