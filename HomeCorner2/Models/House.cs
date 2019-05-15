ad0dba659c6ecef11a9a644f81426b532068a174
Billpapos <vpapoglou@gmail.com>
2019-05-15 10:32:53 +0300

    Restrictions

--- HomeCorner2/Models/House.cs
+++ HomeCorner2/Models/House.cs
@@ -16,27 +16,32 @@ namespace HomeCorner2.Models
 
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Please, provide a Title")]
         public string Title { get; set; }
 
-        //[Required(ErrorMessage = "Please, provide a description")]
+        [Required(ErrorMessage = "Please, provide a Description")]
         public string Description { get; set; }
 
-        //[Required(ErrorMessage = "Please, provide a Region")]
-        //public string Region { get; set; }
-
-        //[Required(ErrorMessage = "Please, provide an Address")]
+        [Required(ErrorMessage = "Please, provide an Address")]
         public string Address { get; set; }
 
+        [Required(ErrorMessage = "Please, provide an Address Number")]
         public string AddressNumber { get; set; }
 
-        public int PostalCode { get; set; }
+        [Required(ErrorMessage = "Please, provide a Postal Code")]
+        [StringLength(5, ErrorMessage = "The Postal Code must be 5 characters long.", MinimumLength = 5)]
+        public string PostalCode { get; set; }
 
+        [Required(ErrorMessage = "Please, provide a Occupancy")]
         public int Occupancy { get; set; }
 
+        [Required(ErrorMessage = "Please, provide a Price")]
         public decimal Price { get; set; }
 
+        [Required(ErrorMessage = "Please, provide Availability")]
         public DateTime StartDate { get; set; }
 
+        [Required(ErrorMessage = "Please, provide Availability")]
         public DateTime EndDate { get; set; }
 
 
--- HomeCorner2/Models/Reservation.cs
+++ HomeCorner2/Models/Reservation.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -12,7 +13,10 @@ namespace HomeCorner2.Models
         public int Id { get; set; }
         public ApplicationUser User { get; set; }
         public House House { get; set; }
+
+        [Required(ErrorMessage = "Please, provide Availability")]
         public DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "Please, provide Availability")]
         public DateTime EndDate { get; set; }
     }
 }
